7d94a257ec0099be316bf67138c45ca43ec7865b
Bayan Derbas <mooonlightexo@gmail.com>
2025-04-25 15:06:35 +0300

    create class colors

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,3 +1,15 @@
-class colors{
+import 'dart:ui';
+import 'package:flutter/material.dart';
 
+class Colors {
+  static const Color purple = Color(0xFFA772EB);
+  static const Color lavender = Color.fromRGBO(206, 175, 250, 1.0);
+  static const Color softPink = Color(0xFFFFB9C3);
+  static const Color blushRose = Color(0xFFFFC8DC);
+  static const Color aquaBlue = Color.fromRGBO(128, 210, 217, 1.0);
+  static const Color babySky = Color.fromRGBO(162, 210, 255, 1.0);
+  static const Color skyBlue = Color.fromRGBO(144, 201, 255, 1.0);
+  static const Color deepNavy = Color(0xFF05293C);
+  static const Color pureWhite = Color(0xFFFFFFFF);
+  static const Color goldenYellow = Color(0xFFFFE289);
 }
